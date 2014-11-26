9df5095e7d92b70f3b05d65a7013cf1448f264e2
Clayton Wallace <clayto1987@gmail.com>
2014-11-26 07:58:04 -0600

    Create and write to database and read in from apache log file complete next step is to combine the program to do both together

--- process_log.rb
+++ process_log.rb
@@ -4,10 +4,11 @@ File.open("access_short.log", "r") do |f|
   f.each_line do |line|
     count = count + 1
     puts line
-    my_string_array = line.sub('[','"').sub(']','"').scan(/"[^"]+"|\S+/).map { |word| word.delete('"') }
+    my_string_array = line.scan(/"[^"]+"|\[[^\[\]]+\]|\S+/).map { |word| word.delete('"') }
     puts my_string_array.inspect
     puts my_string_array.count
   end
 end
 
 puts count
+
